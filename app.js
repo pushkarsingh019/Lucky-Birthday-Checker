e052358a2a2e8636bcf7c03da17329aa8941c798
Pushkar Singh <stoazest@gmail.com>
2022-01-11 08:47:43 +0530

    Completed the Logic of Program

--- app.js
+++ app.js
@@ -4,27 +4,54 @@ var luckyNum  = document.querySelector("#lucky")
 var buttton = document.querySelector("#btn");
 var output = document.querySelector("#output");
 
-// // Making varibales to store the data inputted from the user
-// let birthdayString = birthday.value;
-// let luckyNumString = luckyNum.value;
-var dateArray = [];
 
-// Defining the functions
+// Functions
 
-function convertArray(date){
-    var dateString = date;
-    for (var i=0; i<date.length; i++){ 
-        
-        dateArray.push(+dateString.charAt(i));
-       
+// To find the sum of the digits in the birthday
+function convertString(birthdayValue){
+    let sum = 0
+    for(let index=0; index<birthdayValue.length; index++){
+        sum = sum + Number(birthdayValue.charAt(index))
+    }
+    return sum
+}
+
+// To check if the birthday is lucky or not
+function luckyBirthdayChecker(sum, luckyNumber){
+    if (sum % luckyNumber == 0){
+        return 1
+    }
+    else if (sum % luckyNumber != 0){
+        return 0
+    }
+    else {
+        console.log("something went wrong" + "\n" + "the value is " + sum % luckyNumber )
+    }
+}
+
+// Function to Disply Output
+function displayOutput(flag){
+    if (flag === 1){
+        alert("You are lucky")
+    }
+    else if (flag === 0){
+        alert("you are not lucky")
+    }
+    else {
+        alert("something went wrong")
     }
 }
 
 
-
-
+// Original Event Handler
 function eventHandler(){
-    convertArray(birthday.value);
+
+    // Using ReplaceAll to replace all the special characters in the string
+    let birthdayString = birthday.value.replaceAll("-","");
+    birtdhaySum = convertString(birthdayString);
+    let flag  = luckyBirthdayChecker(birtdhaySum, luckyNum.value);
+    displayOutput(flag)
+    
 }
 
 
---
+++ challenges.md
@@ -0,0 +1,11 @@
+
+# Technical Challenges
+
+### Converting the date String to a string with no hyphens
+
+- The output from the input element in HTML was in this format YYYY-MM-DD. 
+- I needed the output without in Hyphens.
+- I solved with by using replaceAll Function in Javascript, the syntax is 
+    <!-- varibaleName.replaceAll("to be replaced", "to be replaced with") -->
+
+### Adding the sum of all the characters in the birthday String
